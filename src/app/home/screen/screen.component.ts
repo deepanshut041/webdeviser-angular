d3ae2149ee481d2d4f4d62a7599abee366e85abc
deepanshut041 <deepanshut041@gmail.com>
2017-10-15 12:10:19 +0530

    intial demo for hosting is ready....

--- src/app/home/screen/screen.component.ts
+++ src/app/home/screen/screen.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class ScreenComponent {
 
+    question(i:number){
+        var elm1 = document.getElementById("question-1");
+        var elm2 = document.getElementById("question-2");
+        var elm3 = document.getElementById("question-3");
+        if(i == 1){
+            elm1.style.display = "block"
+            elm2.style.display = "none"
+            elm3.style.display = "none"
+        }
+        if(i == 2){
+            elm1.style.display = "none"
+            elm2.style.display = "block"
+            elm3.style.display = "none"
+        }
+        if(i == 3){
+            elm1.style.display = "none"
+            elm2.style.display = "none"
+            elm3.style.display = "block"
+        }
+    }
 }
